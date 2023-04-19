b3b5dcf29b89c0aaa5ef420b6a2637d084dbb604
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-04-19 15:25:46 +0530

    single number

---
+++ singleNumber.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int a=0;
+        
+        for(int i=0;i<nums.length;i++){
+            int c=0;
+            for(int j=0;j<nums.length;j++){
+                if(j==i){
+                    continue;
+                }
+                else if(nums[i]==nums[j]){
+                    c++;
+
+                }
+            }
+            if(c==0){
+                a=nums[i];
+                break;
+            }
+        }
+        return a;
+        
+    }
+}
