4adcce1234408d2661fd298bde5d11385c25c4c9
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-05-08 18:39:28 +0530

    linked list cycle

---
+++ linkedListCycle.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head==null){
+            return false;
+        }
+        ListNode hare=head;
+        ListNode turtle=head;
+        while(hare!=null && hare.next!=null){
+            hare=hare.next.next;
+            turtle=turtle.next;
+            if(hare==turtle){
+                return true;
+            }
+
+        }
+
+        return false;
+        
+    }
+}
