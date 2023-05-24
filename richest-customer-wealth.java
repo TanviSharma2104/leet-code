1077da298e89a1f7c2c3be0e1c78ef69c3d6e421
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-05-24 11:46:16 +0530

    richest-customer-wealth

--- richest-customer-wealth.java
+++ richest-customer-wealth.java
@@ -10,3 +10,22 @@ class Solution {
         return ans[accounts.length-1];
     }
 }
+
+//or
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        //int[] ans=new int[accounts.length];
+        int max=-1;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[0].length;j++){
+                //ans[i]+=accounts[i][j];
+                sum+=accounts[i][j];
+            }
+            max=Math.max(max,sum);
+        }
+        // Arrays.sort(ans);
+        // return ans[accounts.length-1];
+        return max;
+    }
+}
