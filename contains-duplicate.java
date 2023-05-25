c5a3755bee25787bbb8d25bbe7aae4fbea4e40fb
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-05-25 09:57:51 +0530

    Create contains-duplicate

---
+++ contains-duplicate.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }
+            else{
+                map.put(nums[i],1);
+            }
+        }
+        boolean flag=false;
+        for(int key:map.keySet()){
+            if(map.get(key)>=2){
+                flag=true;
+                break;
+            }
+        }
+        return flag;
+    }
+}
