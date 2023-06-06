d8beb15a3a4b9568b021455e6effab0a56e80c6a
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-06-06 18:50:01 +0530

    Create squares-of-a-sorted-array

---
+++ squares-of-a-sorted-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+    //    int[] ans=new int[nums.length];
+    //    for(int i=0;i<nums.length;i++){
+    //        ans[i]=nums[i]*nums[i];
+    //    } 
+    //    Arrays.sort(ans);
+    //    return ans;
+
+
+       for(int i=0;i<nums.length;i++){
+           nums[i]*=nums[i];
+       } 
+       Arrays.sort(nums);
+       return nums;
+    }
+}
