7f716488eba592b714f5996605915b1b0f43e649
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-06-18 16:36:56 +0530

    Create isomorphic-strings

---
+++ isomorphic-strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        HashMap<Character,Character> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char a=s.charAt(i);
+            char b=t.charAt(i);
+            if(map.containsKey(a) && map.get(a)!=b){
+                return false;
+            }else if(!map.containsKey(a) && map.containsValue(b)){
+                return false;
+            }
+            map.put(a,b);
+        }
+        return true;
+    }
+}
