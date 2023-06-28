27dc13883c1d8bbbbb071e4d7b5e97be4afd3eba
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-06-28 23:28:13 +0530

    Create check-if-the-sentence-is-pangram

---
+++ check-if-the-sentence-is-pangram.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        if(sentence.length()<26){
+            return false;
+        }
+        HashSet<Character> set=new HashSet<>();
+        for(int i=0;i<sentence.length();i++){
+            set.add(sentence.charAt(i));
+            if(set.size()==26){
+                return true;
+            }
+            
+        }
+        return false;
+
+      //or
+
+        for (char ch = 'a' ; ch <= 'z' ; ch++){
+            if (sentence.indexOf(ch) < 0)
+                return false;
+        }
+        return true;
+    }
+}
