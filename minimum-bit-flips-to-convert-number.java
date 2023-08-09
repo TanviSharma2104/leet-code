c413e65ea729b0f5d52cd3ba89b1160a6821c56c
TanviSharma2104 <101709149+TanviSharma2104@users.noreply.github.com>
2023-08-09 17:44:27 +0530

    Create minimum-bit-flips-to-convert-number

---
+++ minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        int c=0;
+        while(start>0 | goal>0){
+            if((start&1)!=(goal&1)){
+                c++;
+            }
+            start=start>>1;
+            goal=goal>>1;
+        }
+        return c;
+    }
+}
